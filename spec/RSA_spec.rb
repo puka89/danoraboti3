4b1a0046a91021eb2cc320cc62a862d26215455c
puka89 <ludiat.lud@abv.bg>
2017-12-13 02:53:42 +0300

    Update RSA_spec.rb

--- spec/RSA_spec.rb
+++ spec/RSA_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe RSA do
  
  msg = "danoraboti3"
  key_gen = rsa.new_key
- rsa= RSA.new(new_keys[0], new_keys[1], new_keys[2])
+ rsa = RSA.new(new_keys[0], new_keys[1], new_keys[2])
 
   it "checks if it works" do
    expect(rsa.decrypt(rsa.encrypt(msg))).to eq(msg)
